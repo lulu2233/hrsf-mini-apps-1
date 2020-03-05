47d38d16780ef56a8f18c003a3f1c97e49681c79
lulu2233 <ninglv111@gmail.com>
2020-03-04 19:55:41 -0800

    set up environment

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,13 @@
+class App extends React.Component{
+  render() {
+    return(
+      <div>'hello world'</div>
+    )
+  }
+}
 
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+)
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const app = express();
+const prot = 3000;
+const path = require('path');
 
+app.use('/', express.static(path.join('public')))
+app.listen(port, ()=>{
+  console.log(`Listening on port ${port}!`);
+})
 
